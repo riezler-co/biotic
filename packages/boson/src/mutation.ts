eb8d134cd1af8cf2b3b952e041b4b963c94ff4c4
Michael Riezler <michael@riezler.co>
2021-09-06 00:46:33 +0200

    types

--- packages/boson/src/mutation.ts
+++ packages/boson/src/mutation.ts
@@ -3,16 +3,18 @@ import { ObservableInput, from, Subscription } from 'rxjs'
 import { Boson } from './boson'
 import { useBoson } from './hooks'
 
-export interface UseMutation<T extends Array<any>, Error> {
-	(...args: T): void;
+type Response<Data> = void | null | undefined | Data 
+
+export interface UseMutation<T extends Array<any>, Data, Error> {
+	(...args: T): Promise<Response<Partial<Data>>>;
 	loading: boolean;
 	error: Error | null
 }
 
 export function useMutation<Error=any, Args extends Array<any>=any, Data=unknown>(
 	boson: Boson<Data>,
-	fn: (data: Data, ...args: Args) => ObservableInput<null | undefined | Partial<Data>>,
-): UseMutation<Args, Error> {
+	fn: (data: Data, ...args: Args) => ObservableInput<Response<Partial<Data>>>,
+): UseMutation<Args, Data, Error> {
 	let [loading, setLoading] = useState<boolean>(false)
 	let [error, setError] = useState<Error | null>(null)
 	let [data, setData] = useBoson(boson)
@@ -37,29 +39,81 @@ export function useMutation<Error=any, Args extends Array<any>=any, Data=unknown
 	let run = useCallback((...args: Args) => {
 		setError(null)
 		setLoading(true)
-
-		unsubscribe.current = from(_fn.current(_data.current, ...args)).subscribe({
-			next: (response) => {
-				if (response === undefined || response ===  null) {
-					return
-				}
-
-				setData(currentState => {
-					return {
-						...currentState,
-						...response,
+		return new Promise<Response<Partial<Data>>>((resolve, reject) => {
+			unsubscribe.current = from(_fn.current(_data.current, ...args)).subscribe({
+				next: (response) => {
+					if (response === undefined || response ===  null) {
+						resolve(response)
+						return
 					}
-				})
-				setLoading(false)
-			},
 
-			error: (err) => {
-				setError(err)
-				setLoading(false)
-			},
+					setData(currentState => {
+						return {
+							...currentState,
+							...response,
+						}
+					})
+					setLoading(false)
+					resolve(response)
+				},
+
+				error: (err) => {
+					setError(err)
+					setLoading(false)
+					reject(err)
+				},
+			})
 		})
 
 	}, [setData, setLoading, setError])
+
+	return Object.assign(run, { error, loading })
+}
+
+
+export interface UsePost<T extends Array<any>, Data, Error> {
+	(...args: T): Promise<Response<Data>>;
+	loading: boolean;
+	error: Error | null
+}
+
+export function usePost<Data=unknown, Error=any, Args extends Array<any>=any>(
+	fn: (...args: Args) => ObservableInput<Response<Data>>,
+): UsePost<Args, Data, Error> {
+	let [loading, setLoading] = useState<boolean>(false)
+	let [error, setError] = useState<Error | null>(null)
+
+	let _fn = useRef(fn)
+	useEffect(() => {
+		_fn.current = fn
+	})
+
+	let unsubscribe = useRef<Subscription | null>(null)
+	useEffect(() => () => {
+		if (unsubscribe.current) {
+			unsubscribe.current.unsubscribe()
+		}
+	}, [])
+
+	let run = useCallback((...args: Args) => {
+		setError(null)
+		setLoading(true)
+		return new Promise<Response<Data>>((resolve, reject) => {
+			unsubscribe.current = from(_fn.current(...args)).subscribe({
+				next: (response) => {
+					setLoading(false)
+					resolve(response)
+				},
+
+				error: (err) => {
+					setError(err)
+					setLoading(false)
+					reject(err)
+				},
+			})
+		})
+
+	}, [setLoading, setError])
 
 	return Object.assign(run, { error, loading })
 }
